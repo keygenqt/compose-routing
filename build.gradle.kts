0cbe7e60275fbe569e85ead94e27f3e329c470d7
Margarita <margo.himera@yandex.ru>
2021-12-01 20:20:10 +0300

    Surf init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,36 @@
+buildscript {
+
+    val kotlinVersion: String by project
+    val gradleVersion: String by project
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:$gradleVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    `maven-publish`
-    id("com.jfrog.artifactory")
     id("com.diffplug.spotless")
 }
 
-version = "0.0.1"
-group = "com.keygenqt.routing"
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
 
-spotless {
-    kotlin {
-        target("**/*.kt")
-        licenseHeaderFile("$buildDir/../LICENSE")
-    }
-}
-
-publishing {
-    publications {
-        register("aar", MavenPublication::class) {
-            groupId = group.toString()
-            artifactId = project.name
-            artifact("$buildDir/outputs/aar/compose-routing-$version-debug.aar")
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            licenseHeaderFile(file("${project.rootDir}/spotless/LicenseHeader"))
         }
     }
 }
 
-artifactory {
-    setContextUrl("https://artifactory.keygenqt.com/artifactory")
-    publish {
-        repository {
-            setRepoKey("open-source")
-            setUsername(findProperty("arusername").toString())
-            setPassword(findProperty("arpassword").toString())
-        }
-        defaults {
-            publications("aar")
-            setPublishArtifacts(true)
-        }
-    }
-}
-
-android {
-
-    compileSdk = 30
-
-    defaultConfig {
-        minSdk = 23
-        targetSdk = 31
-        setProperty("archivesBaseName", "compose-routing-$version")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
--- routing/src/main/kotlin/ru/surfstudio/compose/routing/NavScreen.kt
+++ routing/src/main/kotlin/ru/surfstudio/compose/routing/NavScreen.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Vitaliy Zarubin
+ * Copyright 2021 Surf LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
  
-package com.keygenqt.routing
+package ru.surfstudio.compose.routing
 
 interface NavScreen {
     val route: String
