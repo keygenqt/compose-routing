3bcb498cd9888e1df2d9f430eb9e85fbff8a6cb7
Keygenqt <keygenqt@gmail.com>
2022-10-12 19:35:49 +0300

    Update android versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    `maven-publish`
-    id("com.jfrog.artifactory")
     id("com.diffplug.spotless")
 }
-
-val kotlinVersion: String = findProperty("kotlinVersion") as? String ?: "1.6.0"
-val accompanistVersion: String = findProperty("accompanistVersion") as? String ?: "0.22.0-rc"
-val lifecycleVersion: String = findProperty("lifecycleVersion") as? String ?: "2.4.0"
 
 version = "0.0.3"
 group = "com.keygenqt.routing"
@@ -20,38 +14,13 @@ spotless {
     }
 }
 
-publishing {
-    publications {
-        register("aar", MavenPublication::class) {
-            groupId = group.toString()
-            artifactId = project.name
-            artifact("$buildDir/outputs/aar/compose-routing-$version-debug.aar")
-        }
-    }
-}
-
-artifactory {
-    setContextUrl("https://artifactory.keygenqt.com/artifactory")
-    publish {
-        repository {
-            setRepoKey("open-source")
-            setUsername(findProperty("arusername").toString())
-            setPassword(findProperty("arpassword").toString())
-        }
-        defaults {
-            publications("aar")
-            setPublishArtifacts(true)
-        }
-    }
-}
-
 android {
 
-    compileSdk = 30
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 23
-        targetSdk = 31
+        targetSdk = 33
         setProperty("archivesBaseName", "compose-routing-$version")
     }
 
@@ -60,7 +29,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.composeVersion.get()
+        kotlinCompilerExtensionVersion = findProperty("composeCompilerVersion").toString()
     }
 
     buildFeatures {
@@ -69,8 +38,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-    implementation("com.google.accompanist:accompanist-pager:$accompanistVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("com.google.accompanist:accompanist-pager:0.22.0-rc")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("androidx.navigation:navigation-compose:2.4.0-rc01")
 }
